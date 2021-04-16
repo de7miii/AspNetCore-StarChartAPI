3e491a5cebb716e466a55dd932a929128163f4d0
Abdulrahman Daffallah <daffallahabdulrahman@gmail.com>
2021-04-17 00:03:27 +0300

    Get Methods

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,5 +18,55 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var cObject = from o in _context.CelestialObjects
+                where o.Id.Equals(id)
+                select o;
+            if (!cObject.Any()) return NotFound();
+            var satellites = from s in _context.CelestialObjects
+                where s.OrbitedObjectId.Equals(id)
+                select s;
+            if (satellites.Any())
+            {
+                cObject.First().Satellites = satellites.ToList();
+            }
+
+            return Ok(cObject.First());
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestial = _context.CelestialObjects.FirstOrDefault(c => c.Name.Equals(name));
+            if (celestial == null) return NotFound();
+            var satellites = _context.CelestialObjects
+                .Where(o => o.OrbitedObjectId.Equals(celestial.Id));
+            if (satellites.Any())
+            {
+                celestial.Satellites = satellites.ToList();
+            }
+            return Ok(celestial);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestials = _context.CelestialObjects;
+            if (celestials == null) return NotFound();
+            foreach (var celestialObject in celestials)
+            {
+                var satellites = _context.CelestialObjects
+                    .Where(c => c.OrbitedObjectId.Equals(celestialObject.Id));
+                if (satellites.Any())
+                {
+                    celestialObject.Satellites = satellites.ToList();
+                }
+            }
+
+            return Ok(celestials.ToList());
+        }
     }
 }
