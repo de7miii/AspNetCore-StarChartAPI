2c00cf43358225dae6d7eb3774c7aa6428677c0a
Abdulrahman Daffallah <daffallahabdulrahman@gmail.com>
2021-04-16 23:01:58 +0300

    Create Data Models

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -14,15 +14,16 @@ namespace StarChart
         {
             services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
-            services.AddControllers(c =>
-            {
-                c.EnableEndpointRouting = false;
-            });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseEndpoints(c =>
+            {
+                c.MapControllers();
+            });
             app.UseMvc();
         }
     }
